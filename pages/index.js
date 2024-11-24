982b1520865c72668451b75a037c7249b2f055c4
Kshitij Rangari <krangari0392@gmail.com>
2024-11-24 09:31:33 -0500

    auth0 setup and route setup

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
+import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
 export default function Home() {
-  return <div />;
+  const { user } = useUser();
+  console.log("User:", user);
+
+  return (
+    <div>
+      <h1> This is a home page</h1>
+      <div>
+        <Link href="/api/auth/login">Login</Link>
+      </div>
+    </div>
+  );
 }
---
+++ pages/token-topup.js
@@ -0,0 +1,7 @@
+export default function TokenPopup() {
+  return (
+    <div>
+      <h1> This is a TokenPopup Page</h1>
+    </div>
+  );
+}
