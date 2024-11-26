376dfaa15d09251d2e223b5caffe3cb8292a05d2
Kshitij Rangari <krangari0392@gmail.com>
2024-11-26 16:38:51 -0500

    implemented openAI API Handler

--- pages/post/new.js
+++ pages/post/new.js
@@ -1,11 +1,29 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { AppLayout } from "../../components/AppLayout";
+import { useState } from "react";
+import Markdown from "react-markdown";
 
 export default function NewPost(props) {
-  console.log("New post props", props);
+  const [postContent, setPostContent] = useState("");
+  const handleClick = async () => {
+    const response = await fetch("/api/generatePost", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    const json = await response.json();
+    setPostContent(json.postContent);
+  };
+
   return (
     <div>
       <h1> This is a New post page</h1>
+      <button className="btn" onClick={handleClick}>
+        {" "}
+        Generate{" "}
+      </button>
+      <Markdown>{postContent}</Markdown>
     </div>
   );
 }
